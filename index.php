cac51d59f7d9a291a86ff9d82bd6e39cb35ab0ee
Vincent Cassiau <vincentcassiaupro@gmail.com>
2018-06-20 16:35:09 +0200

    Resolution de bugs et changement d'organisation des fichiers

--- index.php
+++ index.php
@@ -2,15 +2,15 @@
 <html lang="fr">
 <head>
     <meta charset="UTF-8">
-    <link rel="stylesheet" href="assets/CSS/index.css" />
-    <link rel="stylesheet" href="assets/CSS/header.css" />
-    <link rel="stylesheet" href="assets/CSS/carrousel.css" />
+    <link rel="stylesheet" href="index.css" />
+    <link rel="stylesheet" href="header.css" />
+    <link rel="stylesheet" href="carrousel.css" />
     <title>Dans Ton Ciné</title>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 </head>
 <body>
 
-<?php include('header.php'); ?>
+<?php include('header.html'); ?>
 
     <div id="boiteCarrousel">
         <div id="boite">
@@ -19,12 +19,6 @@
             </div>
             <div class="container">
                 <div class="inner">
-                    <div class="card"></div>
-                    <div class="card"></div>
-                    <div class="card"></div>
-                    <div class="card"></div>
-                    <div class="card"></div>
-                    <div class="card"></div>
                 </div>
             </div>
             <div class="btn next">
@@ -33,7 +27,6 @@
         </div>
     </div>
 
-<script type="text/javascript" src="ApiIMDB.js"></script>
 <script type="text/javascript" src="assets/JS/carrousel.js"></script>
 </body>
 </html>
--- src/components/Popular.js
+++ src/components/Popular.js
@@ -5,7 +5,6 @@
     theMovieDb.movies.getPopular({},
         function (data) {
             let popularFilmObject = JSON.parse(data)
-            console.log(popularFilmObject)
             for (element in popularFilmObject.results) {
                 $('<div/>', {'class': 'card', html: popularFilmObject.results[element].title}).appendTo($('div.inner')).append($('<img/>', {
                     "src": "https://image.tmdb.org/t/p/w500" + popularFilmObject.results[element].poster_path
