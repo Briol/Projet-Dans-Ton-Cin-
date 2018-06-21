401d43a449c9624f57202bd253f367e8e1b2820c
Vincent Cassiau <vincentcassiaupro@gmail.com>
2018-06-21 11:44:37 +0200

    Modification de compatibilité en vue des modifs fait sur le pc d'Antoine avec lui

--- src/components/Popular.js
+++ src/components/Popular.js
@@ -1,14 +1,15 @@
-    function errorCB(){
-        console.log("non");
-    }
+function errorCB ()
+{
+    console.log('non')
+}
 
-    theMovieDb.movies.getPopular({},
-        function (data) {
-            let popularFilmObject = JSON.parse(data)
-            for (element in popularFilmObject.results) {
-                $('<div/>', {'class': 'card', html: popularFilmObject.results[element].title}).appendTo($('div.inner')).append($('<img/>', {
-                    "src": "https://image.tmdb.org/t/p/w500" + popularFilmObject.results[element].poster_path
-                }))
-            }
-        }, errorCB)
-
+theMovieDb.movies.getPopular({},
+    function (data) {
+        let popularFilmObject = JSON.parse(data)
+        for (element in popularFilmObject.results) {
+            $('<div/>', {'class': 'card'}).appendTo(
+                $('div.inner')).append($('<img/>', {
+                'src': 'https://image.tmdb.org/t/p/w500' + popularFilmObject.results[element].poster_path,
+            }))
+        }
+    }, errorCB)
